69ade7672307b3c1a81e32adbd8bca3cba8a4c00
Andy Staudinger <info@the-freelancer-marketing.com>
2025-06-10 13:44:37 +0200

    test3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 // import { Geist, Geist_Mono } from 'next/font/google'; // AUSKOMMENTIERT
-import { Providers } from '../contexts/providers';
+
 
 /*
 const geistSans = Geist({
