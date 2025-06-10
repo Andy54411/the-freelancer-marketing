40c00850cbb8b67ee1518fc9ae6e9701688b5865
Andy Staudinger <info@the-freelancer-marketing.com>
2025-06-10 14:05:07 +0200

    test2

---
+++ src/app/providers.tsx
@@ -0,0 +1,19 @@
+///Users/andystaudinger/Tasko/src/app/providers.tsx
+'use client';
+
+import { ReactNode } from 'react';
+import { AuthProvider } from '@/contexts/AuthContext';
+import { RegistrationProvider } from '@/contexts/registrationContext';
+import { GoogleMapsLoaderProvider } from '@/contexts/GoogleMapsLoaderContext';
+
+export function Providers({ children }: { children: ReactNode }) {
+    return (
+        <GoogleMapsLoaderProvider>
+            <AuthProvider>
+                <RegistrationProvider>
+                    {children}
+                </RegistrationProvider>
+            </AuthProvider>
+        </GoogleMapsLoaderProvider>
+    );
+}
