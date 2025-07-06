0bc4ae4f0b78b75c9503a13d9dc00ebd155869e0
Andy Staudinger <info@the-freelancer-marketing.com>
2025-07-06 22:23:15 +0200

    update admin

--- src/app/dashboard/admin/support/page.tsx
+++ src/app/dashboard/admin/support/page.tsx
@@ -1,20 +1,19 @@
 import React, { Suspense } from 'react';
 import { FiLoader } from 'react-icons/fi';
-import AdminLayout from './components/AdminLayout';
 import AdminSupportChat from './components/AdminSupportChat';
 
 export const dynamic = "force-dynamic";
 
 export default function Page() {
     return (
-        <AdminLayout>
-            {/* 
-              Suspense ist notwendig, da AdminSupportChat `useSearchParams` verwendet.
-              Dadurch kann der Rest der Seite auf dem Server gerendert werden, während diese Komponente auf dem Client geladen wird.
+        <>
+            {/*
+                Suspense ist notwendig, da AdminSupportChat `useSearchParams` verwendet.
+                Dadurch kann der Rest der Seite auf dem Server gerendert werden, während diese Komponente auf dem Client geladen wird.
             */}
-            <Suspense fallback={<div className="flex h-full items-center justify-center"><FiLoader className="animate-spin text-2xl" /></div>}>
+            <Suspense fallback={<div className="flex h-full items-center justify-center"><FiLoader className="animate-spin text-2xl text-teal-500" /></div>}>
                 <AdminSupportChat />
             </Suspense>
-        </AdminLayout>
+        </>
     );
 }
