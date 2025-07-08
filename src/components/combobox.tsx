a95a37d4ffe90ee7e5abc74c32c1458ed8f51423
Andy Staudinger <info@the-freelancer-marketing.com>
2025-07-08 21:34:46 +0200

    update src/components/combobox.tsx

--- src/components/combobox.tsx
+++ src/components/combobox.tsx
@@ -31,15 +31,7 @@ export function Combobox({ options, placeholder = 'Wähle eine Option', selected
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      {/*
-        FINALE ANPASSUNG: Die Tailwind-Klasse "z-50" wird hinzugefügt.
-        Dies ist die saubere Methode, um sicherzustellen, dass die Auswahlliste
-        immer über anderen Inhalten (wie z.B. Modal-Hintergründen) angezeigt wird.
-      */}
-      <PopoverContent
-        className="w-full p-0 z-50"
-        onOpenAutoFocus={e => e.preventDefault()}
-      >
+      <PopoverContent className="w-full p-0 z-50">
         <Command>
           <CommandInput placeholder="Suchen..." />
           <CommandEmpty>Keine Ergebnisse gefunden.</CommandEmpty>
