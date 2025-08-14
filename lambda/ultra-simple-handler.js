55a89f15dc37d1ca8c4939aa484a6941f1ddf9f1
Andy Staudinger <info@the-freelancer-marketing.com>
2025-08-14 13:23:56 +0200

    Fixed WebSocket Lambda - Deploy AWS-SDK-Free Version

---
+++ lambda/ultra-simple-handler.js
@@ -0,0 +1,29 @@
+exports.handler = async event => {
+  console.log('Ultra Simple WebSocket Handler - Event:', JSON.stringify(event, null, 2));
+
+  const { routeKey, connectionId, requestContext } = event;
+
+  try {
+    switch (routeKey) {
+      case '$connect':
+        console.log('WebSocket $connect for connection', connectionId);
+        console.log('Query params:', requestContext?.queryStringParameters);
+        return { statusCode: 200, body: 'Connected' };
+
+      case '$disconnect':
+        console.log('WebSocket $disconnect for connection', connectionId);
+        return { statusCode: 200, body: 'Disconnected' };
+
+      case '$default':
+        console.log('WebSocket message for connection', connectionId, 'body:', event.body);
+        return { statusCode: 200, body: 'Message received' };
+
+      default:
+        console.log('Unknown route:', routeKey);
+        return { statusCode: 400, body: 'Unknown route' };
+    }
+  } catch (error) {
+    console.error('WebSocket Handler Error:', error);
+    return { statusCode: 500, body: 'Internal Server Error' };
+  }
+};
